75a20ff77f0a948c9669e089c3af27b26970f2fe
MrSargeant <145541054+MrSargeant@users.noreply.github.com>
2023-09-20 08:01:23 +0000

    Created a basic python program to show Ethan and Jayden how to branch.

---
+++ branch.py
@@ -0,0 +1,5 @@
+#create a program that creates and @elizabethan email from the first name, last name and year they joined
+
+firstname = input("What is your firstname?")
+lastname = input("What is your lastname?")
+year = input("What year did you join?")
